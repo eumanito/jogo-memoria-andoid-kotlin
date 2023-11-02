31a1cc54b4689547af2ffb42b69de49435ccba50
g4bx <gabriel.machado.js@gmail.com>
2023-11-02 19:09:41 -0300

    Adicionando cronometro Variável scale nunca modificada, definido como constante

--- app/src/main/java/com/example/memorygame/activity/AnimatorActivity.kt
+++ app/src/main/java/com/example/memorygame/activity/AnimatorActivity.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import com.example.memorygame.R
 
 class AnimatorActivity : AppCompatActivity() {
+    private lateinit var timeTextView: TextView
+    private lateinit var stopwatch: Stopwatch
 
     lateinit var frontAnimation: AnimatorSet
     lateinit var backAnimation: AnimatorSet
@@ -18,7 +20,7 @@ class AnimatorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.oneflipcard)
 
-        var scale = applicationContext.resources.displayMetrics.density
+        val scale = applicationContext.resources.displayMetrics.density
 
         val cardFront = findViewById<TextView>(R.id.card_front)
         val cardBack = findViewById<TextView>(R.id.card_back)
@@ -29,15 +31,22 @@ class AnimatorActivity : AppCompatActivity() {
         frontAnimation = AnimatorInflater.loadAnimator(applicationContext, R.animator.frontanimator) as AnimatorSet
         backAnimation = AnimatorInflater.loadAnimator(applicationContext, R.animator.backanimator) as AnimatorSet
 
+        var timeView = findViewById<TextView>(R.id.timeView)
+        timeTextView = findViewById(R.id.timeView)
+        stopwatch = Stopwatch(timeTextView)
+        stopwatch.start()
+
         val flipCard = View.OnClickListener {
             isFront = if (isFront) {
                 // Perform the flip animation from front to back
+
                 frontAnimation.setTarget(cardFront)
                 backAnimation.setTarget(cardBack)
                 frontAnimation.start()
                 backAnimation.start()
                 false
             } else {
+                stopwatch.stop()
                 // Perform the flip animation from back to front
                 frontAnimation.setTarget(cardBack)
                 backAnimation.setTarget(cardFront)
@@ -49,6 +58,7 @@ class AnimatorActivity : AppCompatActivity() {
 
         cardFront.setOnClickListener(flipCard)
         cardBack.setOnClickListener(flipCard)
+
     }
 }
 
