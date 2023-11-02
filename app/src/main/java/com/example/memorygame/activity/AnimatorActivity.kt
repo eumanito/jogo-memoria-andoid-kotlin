56fcbf6e7ccbf9bd341248b68be42c3ae3a4b221
g4bx <gabriel.machado.js@gmail.com>
2023-11-02 17:01:45 -0300

    Remove button and update gradle

--- app/src/main/java/com/example/memorygame/activity/AnimatorActivity.kt
+++ app/src/main/java/com/example/memorygame/activity/AnimatorActivity.kt
@@ -2,11 +2,9 @@ package com.example.memorygame.activity
 
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
 import com.example.memorygame.R
 
@@ -24,7 +22,6 @@ class AnimatorActivity : AppCompatActivity() {
 
         val cardFront = findViewById<TextView>(R.id.card_front)
         val cardBack = findViewById<TextView>(R.id.card_back)
-        val flip = findViewById<Button>(R.id.flip_btn) as Button
 
         cardFront.cameraDistance = 8000 * scale
         cardBack.cameraDistance = 8000 * scale
@@ -32,42 +29,21 @@ class AnimatorActivity : AppCompatActivity() {
         frontAnimation = AnimatorInflater.loadAnimator(applicationContext, R.animator.frontanimator) as AnimatorSet
         backAnimation = AnimatorInflater.loadAnimator(applicationContext, R.animator.backanimator) as AnimatorSet
 
-        flip.setOnClickListener{
-            if(isFront)
-            {
-                frontAnimation.setTarget(cardFront);
-                backAnimation.setTarget(cardBack);
-                frontAnimation.start()
-                backAnimation.start()
-                isFront = false
-
-            }
-            else
-            {
-                frontAnimation.setTarget(cardBack)
-                backAnimation.setTarget(cardFront)
-                backAnimation.start()
-                frontAnimation.start()
-                isFront =true
-
-            }
-        }
-
         val flipCard = View.OnClickListener {
-            if (isFront) {
+            isFront = if (isFront) {
                 // Perform the flip animation from front to back
                 frontAnimation.setTarget(cardFront)
                 backAnimation.setTarget(cardBack)
                 frontAnimation.start()
                 backAnimation.start()
-                isFront = false
+                false
             } else {
                 // Perform the flip animation from back to front
                 frontAnimation.setTarget(cardBack)
                 backAnimation.setTarget(cardFront)
                 backAnimation.start()
                 frontAnimation.start()
-                isFront = true
+                true
             }
         }
 
